f91a2b066e939ddbf045ea202f88150799c5e9c3
Andrin Kappeler <kappeand@students.zhaw.ch>
2021-12-14 13:04:00 +0100

    Initial commit

---
+++ src/test/java/ch/zhaw/ecotracker/MainTest.java
@@ -0,0 +1,13 @@
+package ch.zhaw.ecotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
